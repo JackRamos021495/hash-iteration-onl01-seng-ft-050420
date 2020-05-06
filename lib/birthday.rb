1c3f457769306249c8389693d4f995a599be1745
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 19:03:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,12 +8,7 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
 
   birthday_kids.each do |kids, age|
-    if age <= 12
-      puts greeting = true
-    else
-     puts greeting = false
-    end
-    greeting = "Happy Birthday #{kids}! You are now #{age} years old!"
+    "Happy Birthday #{kids}! You are now #{age} years old!"
   end
 
 end
