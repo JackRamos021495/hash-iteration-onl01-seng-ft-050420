a0a47cfd82eed13c3726bf8283221899e97f3ae5
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 19:12:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -19,5 +19,6 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids}! You are now #{age} years old!"
     end
+    puts "You are too old for this."
   end
 end
